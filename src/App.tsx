72c35af5c7842ed165e36961189984b7c0346e4f
Uroš Todorović <todorovic.urosh@gmail.com>
2024-01-26 14:39:25 +0100

    finished project

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import people from './data.js';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+const App = () => {
+	const [index, setIndex] = useState(0);
+	const { name, job, image, text } = people[index];
+
+	const nextPerson = () => {
+		setIndex((prev) => (prev + 1) % people.length);
+	};
+
+	const prevPerson = () => {
+		setIndex((prev) => (prev + people.length - 1) % people.length);
+	};
+
+	const randomPerson = () => {
+		const i = Math.floor(Math.random() * people.length);
+		setIndex(i);
+	};
+
+	return (
+		<main>
+			<article className="review">
+				<div className="img-container">
+					<img src={image} alt={name} className="person-img" />
+					<span className="quote-icon">
+						<FaQuoteRight />
+					</span>
+				</div>
+				<h4 className="author">{name}</h4>
+				<p className="job">{job}</p>
+				<p className="info">{text}</p> *
+				<div className="btn-container">
+					<button>
+						<FaChevronLeft className="prev-btn" onClick={() => prevPerson()} />
+					</button>
+					<button>
+						<FaChevronRight className="next-btn" onClick={() => nextPerson()} />
+					</button>
+				</div>
+				<button className="btn btn-hipster" onClick={() => randomPerson()}>
+					Random person
+				</button>
+			</article>
+		</main>
+	);
+};
+export default App;
